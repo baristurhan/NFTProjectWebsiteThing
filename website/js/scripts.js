06190b5b86240c45e04b7714b98e365230ff012b
e74000 <67838497+e74000@users.noreply.github.com>
2021-06-17 15:15:44 +0100

    Upload updated version of script.js

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -1,38 +1,73 @@
-
-/*
-async function printCoolNumber() {
-	updateStatus('Fetching the cool number...');
-	const chainId = await web3.eth.net.getId();
-	const coolNumber = await window.contract.methods.GetCoolNumber().call();
-	updateStatus(`The cool number is: ${coolNumber}`);
-}
-*/
-
-/*
-async function changeCoolNumber() {
-	const value = Math.floor(Math.random()*100);
-	updateStatus(`Updating the cool number as ${value}`);
+async function buy(id) {
+	console.log("Getting account");
 	const account = await getCurrentAccount();
-	const coolNumber = await window.contract.methods.SetCoolNumber(value).send({ from: account });
-	updateStatus('Updated...');
+	console.log(`Account is ${account}.\nFetching price...`);
+	const price = await getPrice(id);
+	const toSend = web3.utils.toWei(price, "ether");
+	console.log(`Price is ${price} CETH.\nBegin transaction...`);
+	await window.contract.methods.buy(id).send({ from: account, value: toSend });
+	console.log("Transaction attempted...");
+	
 }
-*/
+
+
+async function withdraw() {
+	console.log("Getting account");
+	const account = await getCurrentAccount();
+	console.log(`Account is ${account}.\nFetching balance...`);
+	const balance = await getBalance(account);
+	console.log(`Balance is ${balance} CETH.\nBegin transaction...`);
+	await window.contract.methods.withdraw().send( { from: account } )
+	console.log("Transaction attempted...")
+}
+
+
+async function setTokenPrice(id, price) {
+	console.log("Getting account");
+	const account = await getCurrentAccount();
+	console.log(`Account is ${account}.\nBegin transaction...`);
+	await window.contract.methods.setTokenPrice().send( { from: account } )
+}
+
+
+async function getBalance(address) {
+	return await window.contract.methods.getBalance(address).call();
+}
+
+async function getName(id) {
+	return await window.contract.methods.getName(id).call();
+}
+
+async function getPrice(id) {
+	return await window.contract.methods.getPrice(id).call();
+}
+
+async function getURI(id) {
+	return await window.contract.methods.getURI(id).call();
+}
 
 async function getCurrentAccount() {
 	const accounts = await window.web3.eth.getAccounts();
 	return accounts[0];
 }
 
-function getJSON(path) {
+async function getJSON(path) {
     return fetch(path).then(response => response.json());
+}
+
+async function checkNetwork() {
+	const networkId = await web3.eth.net.getId();
+	if (networkId == 777) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
 async function loadContract() {
 	console.log("Fetching ABI data...");
 	
-	ABI = getJSON("./data/ABI.json");
-	
-	console.log(ABI);
+	ABI = await getJSON("./data/ABI.json");
 	
 	return await new window.web3.eth.Contract(ABI, '0xd9145CCE52D386f254917e481eB44e9943F39138');
 }
@@ -41,12 +76,13 @@ async function loadWeb3() {
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
     window.contract = await loadContract();
-    window.ethereum.enable();
+    window.ethereum.eth_requestAccounts;
     console.log("Web3 Loaded...");
   } else {
   	console.log("Failed to load Web3.")
   }
 }
+
 
 async function load() {
   await loadWeb3();
